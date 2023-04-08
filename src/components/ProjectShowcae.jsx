14c232df1fbc787f0ff07141d46a83a7d272f793
Blue Groneman <hello@bluedojo.dev>
2023-04-08 07:57:20 -0600

    lazy load images

--- src/components/ProjectShowcae.jsx
+++ src/components/ProjectShowcae.jsx
@@ -50,6 +50,7 @@ export function ProjectShowcase() {
                   src={post.imageUrl}
                   alt=""
                   className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+                  loading={"lazy"}
                 />
                 <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
               </div>
